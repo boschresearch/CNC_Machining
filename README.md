a80ca70a8a37a6e2849fb6dc7ea8ab424aa408dc
Mohamed-Ali Tnani <94994104+MATnani@users.noreply.github.com>
2021-12-23 14:56:35 +0100

    added the main files of the repository. data will follow

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# CNC_Machining
-data set for process monitoring on CNC machines
+<!---
+
+    Copyright (c) 2019 Robert Bosch GmbH and its subsidiaries.
+
+-->
+
+# CNC Machining Data 
+
+Please cite this paper if using the dataset and direct any questions regarding the dataset to [Tnani Mohamed-Ali](mailto:mohamed-ali.tnani@boschrexroth.de). The paper can be found at: (the data paper has been submitted to [CIRP CMS](https://www.cirp-cms2022.org) and the link will be added upon publication)
+
+## General information and Context
+The dataset provided is a collection of real-world industrial vibration data collected from a brownfield CNC milling machine. The acceleration has been measured using a tri-axial accelerometer (Bosch CISS Sensor) mounted inside the machine. The X- Y- and Z-axes of the accelerometer have been recorded using a sampling rate equal to 2 kHz. Thereby normal as well as anomoulous data have been collected for 4 different timeframes, each lasting 5 months from February 2019 until August 2021 and labelled accordingly. It can be used to investigate the scalability of models and research process variations as the anomaly impact differs. In total there is data from three different CNC milling machines each executing 15 processes. For a detailed description of the data and experimental set-up, please refer to the paper. 
+
+## Dataset:
+
+The data are located in the `data` folder.
+
+### Folder structure: 
+
+The `data` directory containing the manually annotated machine processes is structured as follows:
+```
+data/
+    machine number/             Corresponding machine number. We have data from 3 machines (M01, M02, M03)
+        process number/         Coresponding process number. We have 15 different processes (OP00, .., OP14)
+            label/              Coresponding process health. "good": Normal vibrational data, "bad": Anomalous vibrational data
+                filename:       .h5 file containing the vibration data. The files are annotated as follows: Machine no. + timeframe + Process no + example no. e.g. M02_Aug_2019_OP03_000.h5
+    
+```
+
+### Data structure: 
+
+The .h5 file contains the label of the process and is structured as follows: 
+```
+   vibration data         the dataset contains a ndarray of dimension (acc_values, n_channels). (n_channels: acceleration axis: 0: X-axis, 1:Y-axis, 2:Z-axis)
+    
+```
+
+## Code
+
+### Installation Requirements
+
+The code provided for loading the data has been written in Python 3.8. You need the following conda packages: `h5py`, `numpy` and `matplotlib`. The script `utils/data_loader_utils.py` proposes functions to load and vizualize the data. The notebook `Data_explorer.ipynb` shows how those functions can be used.
+
+
+## License
+The code in this repository is open-sourced under the BSD-3-Clause license. See the [License](utils/License) file for details.
+
+The dataset created for the research located in the directory [data](data) are licensed under a [Creative Commons Attribution 4.0 International
+License](http://creativecommons.org/licenses/by/4.0/) (CC-BY-4.0).
---
+++ utils/data_loader_utils.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+
+
+
+
+"""
+
+""" 
+# Copyright (c) 2021 Bosch Rexroth AG
+# SPDX-License-Identifier: BSD-3-Clause
+
+Author: Mohamed-Ali Tnani (Mohamed-ali.tnani@boschrexroth.de)
+Date: November 24, 2021
+
+This software is provided by the copyright holders and contributors "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+"""
+
+import os
+import sys
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+def find_all_h5s_in_dir(s_dir):
+    """
+    list all .h5 files in a directory
+    """
+    
+    fileslist=[]
+    for root, dirs, files in os.walk(s_dir):
+        for file in files:
+            if file.endswith(".h5"):
+                 fileslist.append(file)
+    return fileslist
+
+
+def load_tool_research_data(data_path, label, add_additional_label= True, verbose=True):
+    """
+    load data (good and bad) from the research data storages
+    
+    Keyword Arguments:
+            data_path {str} -- [path to the directory] 
+            label {str} -- ["good" or "bad"]
+            add_additional_label {bool} -- [if true the labels will be in the form of "Mxx_Aug20xx_OPxx_sampleNr_label" otherwise "label"] (default: True)
+            verbose {bool}
+
+        Returns:
+            datalist --  [list of the the X samples]
+            label --  [list of the the y labels ]
+    """
+    datalist = []
+    data_label = []
+    
+    #list all .h5 files
+    list_paths = find_all_h5s_in_dir(data_path)
+    list_paths.sort()
+    
+    #read and append the samples with the corresponding labels
+    if verbose:
+        print(f"laoding files from {data_path}... ")
+    for element in list_paths:
+        # check if additional label needed ("Mxx_Aug20xx_Tool,nrX") 
+        if add_additional_label:
+            add_label = element.split('/')[-1]
+            additional_label = add_label[:-3]+"_"+label
+        else: additional_label = label
+        # extract data X and y 
+        with h5py.File(os.path.join(data_path,element),'r') as f:
+            vibration_data = f['vibration_data'][:]
+        datalist.append(vibration_data)
+        data_label.append(additional_label)
+
+    return datalist, data_label
+
+
+def datafile_read (file,plotting = True):
+    """loads and plots the data from the datafile
+
+    Keyword Arguments:
+        file {str} -- [path of the file] 
+
+    Returns:
+        ndarray --  [raw data original]
+    """
+    with h5py.File(file,'r') as f:
+            vibration_data = f['vibration_data'][:]
+    # interpolation for x axis plot
+    freq =  2000
+    samples_s = len(vibration_data[:,0])/freq 
+    samples = np.linspace(0, samples_s ,len(vibration_data[:,0]))
+
+    # plotting
+    if plotting:
+        plt.figure(figsize=(20,5))
+        plt.plot(samples,vibration_data[:,0], 'b')
+        plt.ylabel('X-axis Vibration Data')
+        plt.xlabel('Time [sec]')
+        plt.locator_params(axis='y', nbins=10)
+        plt.grid()
+        plt.show()
+        plt.figure(figsize=(20,5))
+        plt.plot(samples,vibration_data[:,1], 'b')
+        plt.ylabel('Y-axis Vibration Data')
+        plt.xlabel('Time [sec]')
+        plt.locator_params(axis='y', nbins=10)
+        plt.grid()
+        plt.show()
+        plt.figure(figsize=(20,5))
+        plt.plot(samples,vibration_data[:,2], 'b')
+        plt.ylabel('Z-axis Vibration Data')
+        plt.xlabel('Time [sec]')
+        plt.locator_params(axis='y', nbins=10)
+        plt.grid()
+        plt.show()
+    return vibration_data
+
